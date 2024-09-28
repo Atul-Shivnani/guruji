e3584681ebc8d1729af4921975442ff723ae3a47
Atul <shivnaniatul@gmail.com>
2024-09-28 12:53:49 +0530

    undo

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://13.49.66.113:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
 
   
